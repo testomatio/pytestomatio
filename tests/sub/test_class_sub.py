3b9bcef5d60c1d4fe260aa86bc1638b7c1c19024
Tiko <taras@tikoqa.com>
2024-12-08 22:13:38 +0100

    fix: pytestomatio plugin usage with xdist, add tests, sync tests

--- tests/sub/test_class_sub.py
+++ tests/sub/test_class_sub.py
@@ -3,14 +3,17 @@ from pytest import mark
 
 class TestClassSub:
 
+    @mark.testomatio("@T7e1cf6d3")
     def test_one_pass_sub(self):
         x = 'this'
         assert 'h' in x
 
+    @mark.testomatio("@T64c0abec")
     def test_two_fail_sub(self):
         x = 'hello'
         assert hasattr(x, 'check')
 
+    @mark.testomatio("@Ta488bdcb")
     @mark.skip
     def test_three_skip_sub(self):
         x = 'hello'
